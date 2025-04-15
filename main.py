0a27e139d78bcc2a72d27bba3c3f6b2567172c70
pfyy <112098515+pfyy@users.noreply.github.com>
2025-04-16 01:05:54 +0800

    gadget port

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ DST_GADGET_FILENAME = "libflorida.so"
 
 SMALI_PATCH_FILEPATH = "smali.patch"
 
+GADGET_PORT = 10443
+
 
 def clear_last_build():
     os.system(f'rmdir /s /q "{DECODED_APK_DIRPATH}"')
@@ -63,9 +65,40 @@ def sign_apk():
     )
 
 
+OLD_GADGET_ASSEMBLY = bytes.fromhex("43 34 8D 52")
+
+
+def get_new_gadget_assembly():
+    old_gadget_assembly_bin = bin(int.from_bytes(OLD_GADGET_ASSEMBLY[::-1]))[2:].rjust(
+        32, "0"
+    )
+
+    new_gadget_assembly_bin = (
+        old_gadget_assembly_bin[:11]
+        + bin(GADGET_PORT)[2:].rjust(16, "0")
+        + old_gadget_assembly_bin[27:]
+    )
+
+    new_gadget_assembly = int(new_gadget_assembly_bin, 2).to_bytes(4)[::-1]
+
+    return new_gadget_assembly
+
+
+def update_gadget_binary(gadget_binary: bytes):
+    new_gadget_assembly = get_new_gadget_assembly()
+    gadget_binary = gadget_binary.replace(
+        OLD_GADGET_ASSEMBLY,
+        new_gadget_assembly,
+        1,
+    )
+    return gadget_binary
+
+
 def unzip_gadget():
     with lzma.open(SRC_GADGET_FILEPATH) as f:
         gadget_binary = f.read()
+
+    gadget_binary = update_gadget_binary(gadget_binary)
 
     with open(f"{DECODED_APK_DIRPATH}/lib/arm64-v8a/{DST_GADGET_FILENAME}", "wb") as f:
         f.write(gadget_binary)
